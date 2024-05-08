b5d8403a65b80ca220dfa0b90680490cc1e94451
Sami Islam <islam.samipkr@gmail.com>
2024-05-08 18:59:59 -0400

    Update azure_app_service.tf

--- azure_app_service.tf
+++ azure_app_service.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "exampleazureapp" {
 
 resource "azurerm_app_service_plan" "exampleazureappserviceplan" {
   name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.exampleazureapp.location
+  resource_group_name = azurerm_resource_group.exampleazureapp.name
 
   sku {
     tier = "Standard"
@@ -16,9 +16,9 @@ resource "azurerm_app_service_plan" "exampleazureappserviceplan" {
 
 resource "azurerm_app_service" "exampleappserviceplanshow" {
   name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = azurerm_resource_group.exampleazureapp.location
+  resource_group_name = azurerm_resource_group.exampleazureapp.name
+  app_service_plan_id = azurerm_app_service_plan.exampleazureappserviceplan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
