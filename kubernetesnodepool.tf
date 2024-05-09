492aa82b0219019d875f72046478ecaa155641e5
Sami Islam <islam.samipkr@gmail.com>
2024-05-08 20:08:07 -0400

    Update kubernetesnodepool.tf

--- kubernetesnodepool.tf
+++ kubernetesnodepool.tf
@@ -1,14 +1,5 @@
-locals {
-  modified_clusters = [
-    for cluster in var.classworkclusters :
-    substr(cluster, 0, length(cluster) - 7)
-  ]
-}
-output "modified_cluster_output"{
-value =[for x in local.modified_clusters:x]
-}
 resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
- for_each               = local.modified_clusters
+ for_each               = azurerm_kubernetes_cluster.batchabcd
  name                   = each.key
  kubernetes_cluster_id  = each.value.id
  vm_size                = "Standard_DS2_v2"
