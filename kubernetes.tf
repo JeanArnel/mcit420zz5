22cbe90ed8fdb5eb6861be460cf33bbf61bee25c
Sami Islam <islam.samipkr@gmail.com>
2024-05-08 20:13:38 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "azureresourcegroup" {
 
 resource "azurerm_kubernetes_cluster" "batchabcd" {
   for_each            = {for cluster in var.classworkclusters: cluster=>cluster}
-  name                = "${each.key}"
+  name                = "${var.prefix}${each.key}"
   location            = azurerm_resource_group.azureresourcegroup.location
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   dns_prefix          = "exampleaks1"
